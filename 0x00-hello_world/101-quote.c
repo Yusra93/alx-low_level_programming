ad0d25ff0db90efb229e49d05b498aad31474b3a
yusra93 <yusra.altigani@gmail.com>
2023-02-17 06:03:39 -0800

    Another way to print in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ *
+ * Return: 1 if success 
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	return (1);
+}
+
