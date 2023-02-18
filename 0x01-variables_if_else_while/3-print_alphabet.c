337aabe98a369271694467f4749c9a0914cdaf2a
yusra93 <yusra.altigani@gmail.com>
2023-02-18 10:53:53 -0800

    Print alphabet-2

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point. print alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a', cz = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (cz <= 'Z)
+	{
+		putchar(cz);
+		cz++;
+	}
+	putchar('\n');
+	return (0);
+}
+
