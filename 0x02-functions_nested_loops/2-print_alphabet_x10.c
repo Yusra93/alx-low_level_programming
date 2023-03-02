4fa449ecefb55078c39d9d344ef70a38b5f6b842
yusra93 <yusra.altigani@gmail.com>
2023-03-01 20:48:06 -0800

    03

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *print_alphabet -  print alphabet
+ * print_alphabet -  print alphabet 10x
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	char ch = 'a';
 
