141a2d85ac7a0fc58ca9bb36a525bee6e853311a
yusra93 <yusra.altigani@gmail.com>
2023-03-01 20:40:28 -0800

    02

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *print_alphabet -  print alphabet
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
