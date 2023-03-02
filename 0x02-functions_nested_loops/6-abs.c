2d8a94896a10441b50ac04b51d37ad7c12683cc9
yusra93 <yusra.altigani@gmail.com>
2023-03-02 02:40:23 -0800

    06

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _abs - get the absolute 
+ * _abs - get the absolute
  * @n: the integer value it receives
  * Return: the abs no
  */
 int _abs(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
 		return (n);
 	}
 	else
 	{
-		n = -1 * n ;
+		n = -1 * n;
 		return (n);
 	}
 }
