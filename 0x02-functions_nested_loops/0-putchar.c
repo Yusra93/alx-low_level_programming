76d031ed955e15b1bc2fba21539dcf58f7a21f5c
yusra93 <yusra.altigani@gmail.com>
2023-03-01 20:33:52 -0800

    My first task in 0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char str[] =  "_putchar\n";
+	int i = 0;
+
+
+	while (str[i] != '\0')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
+
