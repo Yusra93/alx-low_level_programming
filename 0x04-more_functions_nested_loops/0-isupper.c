fb675a92e0e2185eef8014e7143f773102af19f3
yusra93 <yusra.altigani@gmail.com>
2023-03-24 03:15:51 -0700

    check  if upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * Description: prints the date
+ * @c: character to check
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <=)
+		return(1);
+	else
+		return(0);
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+int _isupper(int);
+
+#endif /* MAIN_H */
