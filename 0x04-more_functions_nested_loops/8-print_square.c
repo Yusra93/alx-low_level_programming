4a947ab74687f9d73d167bbc7b4902d489824b76
yusra93 <yusra.altigani@gmail.com>
2023-03-25 01:56:13 -0700

    Print rectangle!

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - function that prints a square, followed by a new line.
+ * @size: int
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j = 0;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+				_putchar('#');
+		_putchar('\n');
+		}
+	}
+}
+
