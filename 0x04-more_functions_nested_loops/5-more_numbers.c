87ceeb19b7bb4e45271fc51d19230249dcefe768
yusra93 <yusra.altigani@gmail.com>
2023-03-24 17:31:49 -0700

    Print more numbers!

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - function that prints numbers from 0 to 14.
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j = 0;
+
+	while (j < 10)
+	{
+		for (i = 0; i < 15; i++)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+
