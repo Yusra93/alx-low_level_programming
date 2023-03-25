6af5a60add2d92423eb020fbdb15ea2ba9f277e9
yusra93 <yusra.altigani@gmail.com>
2023-03-24 17:27:19 -0700

    Print most numbers!

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,20 +2,22 @@
 #include "main.h"
 
 /**
- * print_most_numbers - function that prints numbers from 0 to 9 execluding 2 and 4.
+ * print_most_numbers - function that prints numbers-
+ * from 0 to 9 execluding 2 and 4.
  * Return: void
  */
 
 void print_most_numbers(void)
 {
-	char i = 0;
+	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		if (i == 2 || i == 4)
+		if ((i == 2) || (i == 4))
+		{
 			continue;
+		}
 		_putchar(i + '0');
-		i++;
 	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_most_numbers - function that prints numbers from 0 to 9 execluding 2 and 4.
+ * Return: void
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/* void print_most_numbers(void)*/
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		if ((i == 2) || (i == 4))
+		{	
+			continue;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+	return(0);
+}
+
