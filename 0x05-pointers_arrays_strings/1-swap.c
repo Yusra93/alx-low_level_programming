5a534c988ad175921ef00e7bab43b1677c48d808
yusra93 <yusra.altigani@gmail.com>
2023-03-29 03:07:57 -0700

    Swap a Pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int : function that swaps the values of two integers.
+ * @a: address
+ * @b: address
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *p1, *p2;
+
+	p1 = a;
+	p2 = b;
+	*p1 = b;
+	*p2 = a;
+}
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,15 +2,6 @@
 #define MAIN_H
 
 int _putchar(char);
-int _isupper(int);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_triangle(int size);
-void print_square(int size);
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
