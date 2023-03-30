5fb423fe4aeae7359db7f369939404ea13740d4f
yusra93 <yusra.altigani@gmail.com>
2023-03-30 01:53:15 -0700

    Print the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,11 +12,12 @@ void puts_half(char *str)
 {
 	int len = strlen(str);
 	int half = len / 2;
+	int i;
 
 	if (len % 2 != 0)
 	half = len - 1 / 2;
 
-	int i = half;
+	i = half;
 
 	while (i < len)
 	{
