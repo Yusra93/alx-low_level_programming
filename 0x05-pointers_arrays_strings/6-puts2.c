6765fa958ee4c0db81abb2989c953bc3e2308629
yusra93 <yusra.altigani@gmail.com>
2023-03-30 01:23:13 -0700

    Print a string, hop on char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - function that prints every other character of a string.
+ * @str: address to string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len = strlen(s);
+	int i = 0;
+
+	while (i+=2)
+	{
+	_putchar(s[i]);
+	i++;
+	}
+}
+
