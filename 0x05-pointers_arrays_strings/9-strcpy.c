a2bb1b36723abfce2fd041cc8a5819807320fe1d
yusra93 <yusra.altigani@gmail.com>
2023-03-30 02:47:41 -0700

    Copy a string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,9 +15,9 @@ char *_strcpy(char *dest, char *src)
 	/* dest = strcpy(src);*/
 	int i;
 	int len = strlen(src);
-	
-	for (i = 0; i < len; i++)
-		*(dest + i) = src[i];
+
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
 	return (dest);
 }
 
