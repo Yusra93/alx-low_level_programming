25730667c1945eccc8f50177073efa1be23f0757
yusra93 <yusra.altigani@gmail.com>
2023-04-10 00:47:09 -0700

    concatenat n digit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - function that concatenates two strings.
+ * @dest: char pointer
+ * @src : char pointer
+ * @n : int
+ * Return: address to char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	/* dest = strncat(src, n);*/
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	return (dest);
+}
+
