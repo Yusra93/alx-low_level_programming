e123bfb7de0ced5929f3fbdef8638a16d52c0660
yusra93 <yusra.altigani@gmail.com>
2023-04-10 00:53:54 -0700

    concatenat n digit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	/* dest = strncat(src, n);*/
 	int i;
 
-	for (i = 0; i < n && src != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[len + i] = src[i];
 	}
