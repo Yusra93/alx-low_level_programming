9e66f48cd4f7f25ab49a0ba6c8f07cc1b01b0f14
yusra93 <yusra.altigani@gmail.com>
2023-04-10 01:09:01 -0700

    copy n digit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - function that copies string.
+ * @dest: char pointer
+ * @src : char pointer
+ * Return: address to char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	/* dest = strcpy(src);*/
+	int i;
+	int len = strlen(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
