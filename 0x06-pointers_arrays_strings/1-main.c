a897772cbc1451fcb168dfabe33d0ccff5620d1a
yusra93 <yusra.altigani@gmail.com>
2023-04-10 01:58:14 -0700

    compare!

--- 0x06-pointers_arrays_strings/1-main.c
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -6,55 +6,15 @@
  * print_most_numbers - function that prints numbers from 0 to 9 execluding 2 and 4.
  * Return: void
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-void print_diagonal(int n)
-{
-	int i, j, l;
 
-        if (n <= 0)
-                _putchar('\n');
-	else
-	{
-        	for (j = 0; j < n; j++)
-        	{
-                	for (i = n-1 ; i > j; i--)
-                	{
-                        	_putchar(' ');
-                	}
-			for( l = 0; l <= j; l++)
-			{
-				_putchar('#');
-			}
-		_putchar('\n');
-        	}
-	}
-}
 int main(void)
 {
-/*	print_diagonal(2);
-   	print_diagonal(10);
-    	print_diagonal(1);
-	print_diagonal(0);
-	print_diagonal(-8);
-	print_diagonal(7);
-*/
-	char s1[98] = "Hello ";
-    char s2[] = "World!\n";
-    char *ptr;
+	char s1[] = "Hello";
+	char s2[] = "World!";
 
-    printf("%s\n", s1);
-    printf("%s", s2);
-    ptr = _strncat(s1, s2, 1);
-    printf("%s\n", s1);
-    printf("%s", s2);
-    printf("%s\n", ptr);
-    ptr = _strncat(s1, s2, 1024);
-    printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
 	return(0);
 }
 
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, n;
+	int i;
 	int len1 = strlen(s1);
-	int len2 = strlen(s2);
+	/* int len2 = strlen(s2);*/
 
 	for (i = 0; i < len1; i++)
 	{
-		if (len1 == len2 && s1[i] > s2[i])
-			n++;
-		else if (s1[i] < s2[i])
-			n--;
-		else
-			n = 0;
+		if (s1[i] != s2[i])
+			break;
 	}
-	return (n);
+	return (s1[0] - s2[0]);
 }
 
