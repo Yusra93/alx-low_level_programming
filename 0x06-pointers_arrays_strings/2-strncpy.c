fec556e14c18457206d0dc7371a06db68342de08
yusra93 <yusra.altigani@gmail.com>
2023-04-10 01:13:37 -0700

    copy n digit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	/* dest = strcpy(src);*/
 	int i;
-	int len = strlen(dest);
+	/*int len = strlen(dest);*/
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = src[i];
-	}
-	dest[i] = '\0';
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
 
