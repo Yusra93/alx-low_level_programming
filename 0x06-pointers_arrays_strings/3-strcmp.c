bf9e7851d39a67dcabda6c4c286ddf665c0846ec
yusra93 <yusra.altigani@gmail.com>
2023-04-10 01:44:04 -0700

    compare!

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,14 +15,12 @@ int _strcmp(char *s1, char *s2)
 	int len1 = strlen(s1);
 	int len2 = strlen(s2);
 
-	if (len1 != len2)
-		return 0;
 	for (i = 0; i < len1; i++)
 	{
-		if (s1[i] > s2[i])
-			n = 5;
+		if (len1 == len2 && s1[i] > s2[i])
+			n++;
 		else if (s1[i] < s2[i])
-			n = -5;
+			n--;
 		else
 			n = 0;
 	}
