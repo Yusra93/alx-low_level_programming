4417488436313b867a0cfcedcd8708206ba6a65b
yusra93 <yusra.altigani@gmail.com>
2023-04-11 04:26:33 -0700

    Capitalize!

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,26 +11,26 @@
 char *cap_string(char *str)
 {
 	int i;
-	int len = strlen(str);
+/*	int len = strlen(str);*/
 
-	for (i = 0;  i < len; i++)
+	for (i = 0;  str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
 			continue;
-		else if(str[i - 1] == '\t' || 
-		    str[i - 1] == ' '  || 
-		    str[i - 1] == '\n' ||
-		    str[i - 1] == '.'  ||
-		    str[i - 1] == ')'  ||
-		    str[i - 1] == '('  ||
-		    str[i - 1] == '{'  ||
-		    str[i - 1] == '}'  ||
-		    str[i - 1] == '!'  ||
-		    str[i - 1] == '?'  ||
-		    str[i - 1] == ','  ||
-		    str[i - 1] == '"'  ||
-		    str[i - 1] == ';'  ||
-		    i == 0)
+		else if (str[i - 1] == '\t' ||
+			str[i - 1] == ' '  ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == '.'  ||
+			str[i - 1] == ')'  ||
+			str[i - 1] == '('  ||
+			str[i - 1] == '{'  ||
+			str[i - 1] == '}'  ||
+			str[i - 1] == '!'  ||
+			str[i - 1] == '?'  ||
+			str[i - 1] == ','  ||
+			str[i - 1] == '"'  ||
+			str[i - 1] == ';'  ||
+			i == 0)
 			str[i] -= 32;
 	}
 	return (str);
