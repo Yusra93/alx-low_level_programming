d0957fb23afebb38d497abe5ae5b7752a857e5c6
Yusra93 <yusra.altigani@gmail.com>
2023-06-08 17:41:41 +0200

    # Recursion 0.4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,9 +13,11 @@ int _pow_recursion(int x, int y)
 {
 	int pow;
 
+	if (y == 0)
+		return (1);
 	if (y < 0)
 		return (-1);
-	if (y != 1)
+	if (y > 0)
 		pow = x * _pow_recursion(x, y - 1);
 	return (pow);
 }
