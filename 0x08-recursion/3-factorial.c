c979e6e176fd8b3dc03375a3088d8b6eb07ca9b9
Yusra93 <yusra.altigani@gmail.com>
2023-06-08 13:04:59 +0200

    # Recursion 0.3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,8 +11,10 @@ int factorial(int n)
 {
 	int fact;
 
-	if (n == 1 || n == 0) 
+	if (n == 1 || n == 0)
 		return (1);
+	if ( n < 0) 
+		return (-1);
 	fact = n * factorial(n - 1);
 
 	return (fact);
