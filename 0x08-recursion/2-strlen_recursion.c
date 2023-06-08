5c28c856f19051ecbf4487393b7ce75ea6921c8e
Yusra93 <yusra.altigani@gmail.com>
2023-06-08 12:55:32 +0200

    # Recursion 0.2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - Write a function that returns the factorial of a given number.
+ * @n: integer number
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	int fact;
+
+	if (n != 1)
+	{
+	fact = n * factorial(n - 1);
+	}
+	return(fact);
+}
