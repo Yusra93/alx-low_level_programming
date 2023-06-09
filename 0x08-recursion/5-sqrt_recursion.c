e531b0e6b67a3e3ce7dc4f27e3503401c2f83558
Yusra93 <yusra.altigani@gmail.com>
2023-06-09 10:15:47 +0200

    # Recursion 0.6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+
+/**
+ * is_prime_number - Write a function that returns 1 if the input integer
+ * is a prime number, otherwise return 0.
+ * @n: integer number
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	int p = sqrt(n);
+
+	if (n == 0 || n == 1)
+		return (0);
+	if (n <= 3)
+		return (1);
+	if (n % p != 0)
+		return (0);
+	p--;
+	return is_prime_number(p);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - Write a function that returns the value of x
+ * raised to the power of y.
+ * @x: integer number
+ * @y: power
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int pow;
+
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+		pow = x * _pow_recursion(x, y - 1);
+	return (pow);
+}
