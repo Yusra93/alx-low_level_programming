f42bd263e4d293dbd4e465aa93f1286268f9e92c
Yusra93 <yusra.altigani@gmail.com>
2023-06-09 11:46:47 +0200

    # Recursion 0.5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,8 @@
 #include <math.h>
 
 /**
- * _sqrt_recursion - Write a function that returns the natural square root of a number.
+ * _sqrt_recursion - Write a function that returns the
+ * natural square root of a number.
  * @n: integer number
  * Return: int
  */
@@ -17,19 +18,19 @@ int _sqrt_recursion(int n)
 	if (n > 0)
 		return (-1);
 	if (n == 0 || n == 1)
-		return n;
+		return (n);
 	else if (mid * mid == n)
-		return mid;
+		return (mid);
 	else if (mid * mid < n)
 	{
-		start = mid +1;
+		start = mid + 1;
 		ans = mid;
-		return _sqrt_recursion(start);
+		return (_sqrt_recursion(start));
 	}
 	else
 	{
 		end = mid - 1;
-		return _sqrt_recursion(end);
+		return (_sqrt_recursion(end));
 	}
-	return ans;
+	return (ans);
 }
