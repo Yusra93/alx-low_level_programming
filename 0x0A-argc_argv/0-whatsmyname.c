a800d17537e3755b991d3ac31894a10b0bf05dbf
Yusra93 <yusra.altigani@gmail.com>
2023-06-10 10:56:32 +0200

    # Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Return: 0 for success
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# Pass an Arguments :)
