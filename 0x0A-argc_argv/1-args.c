8b20372bc7946ca626340fb663763407a9b38810
Yusra93 <yusra.altigani@gmail.com>
2023-06-10 13:41:03 +0200

    # Argv 2.0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @argc : no of parameters
+ * @argv : array of string
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	_putchar(argc + '0');
+	_putchar('\n');
+	return (0);
+}
