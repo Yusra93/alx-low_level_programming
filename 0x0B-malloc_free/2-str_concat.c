ae306a8818832acd0216ff6c0718d6221774c7ca
Yusra93 <yusra.altigani@gmail.com>
2023-06-14 19:29:03 +0200

    # malloc 2.0

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,21 +13,25 @@
 char *str_concat(char *s1, char *s2)
 {
 	size_t  i, j = 0;
-	unsigned int l1 = strlen(s1);
-	unsigned int l2 = strlen(s2);
-	unsigned int l = l1 + l2 + 1;
-	char *x = (char *) malloc(l * sizeof(char));
+	unsigned int l1;
+	unsigned int l2;
+	unsigned int l;
+	char *x;
 
 	if (s1 == NULL)
 	{
 		s1 = (char *) malloc(sizeof(char));
-		s1 = " ";
+		s1 = '\0';
 	}
 	if (s2 == NULL)
-		{
+	{
 		s2 =  (char *) malloc(sizeof(char));
-		s2 = " ";
-		}
+		s2 = '\0';
+	}
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+	l = l1 + l2 + 1;
+	x = (char *) malloc(l * sizeof(char));
 	for (i = 0; i < l; i++)
 	{
 		if (i >= l1)
