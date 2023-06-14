bd2a79591d28111803b7b612e7a68e88f950ace7
Yusra93 <yusra.altigani@gmail.com>
2023-06-14 19:23:20 +0200

    # malloc 2.0

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,12 +21,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 	{
 		s1 = (char *) malloc(sizeof(char));
-		s1[j] = " ";
+		s1 = " ";
 	}
 	if (s2 == NULL)
 		{
 		s2 =  (char *) malloc(sizeof(char));
-		s2[j] = " ";
+		s2 = " ";
 		}
 	for (i = 0; i < l; i++)
 	{
