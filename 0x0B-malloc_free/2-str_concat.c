3a94d8d9ddaf53f39f2dba6b5331cb6911e22f11
Yusra93 <yusra.altigani@gmail.com>
2023-06-14 19:12:27 +0200

    # malloc 2.0

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,7 +6,7 @@
 /**
  * str_concat - Write a function that concatenates two strings.
  * @s1 : string
- * @s3 : string
+ * @s2 : string
  * Return: pointer to string
  */
 
@@ -15,13 +15,15 @@ char *str_concat(char *s1, char *s2)
 	size_t  i, j = 0;
 	unsigned int l1 = strlen(s1);
 	unsigned int l2 = strlen(s2);
-	unsigned int l = l1 + l2;
-	char *x = (char*) malloc(l * sizeof(char));
+	unsigned int l = l1 + l2 + 1;
+	char *x = (char *) malloc(l * sizeof(char));
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
+		s1 = "";
 	}
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; i < l; i++)
 	{
 		if (i >= l1)
