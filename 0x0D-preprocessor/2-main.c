6ed9abd445e404a517c6af608e33346303a13594
Yusra93 <yusra.altigani@gmail.com>
2023-06-19 14:14:24 +0200

    macro 0.2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,12 @@
-#define PI 
+#include<stdio.h>
+#include "/usr/local/include/myheader.h"
+/**
+ * main - main entry
+ * Return : 0 for success
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE_);
+	return (0);
+}
