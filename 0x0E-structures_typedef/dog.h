54cb6fe217bc1107d8a1295110e8fe45ca9435ca
Yusra93 <yusra.altigani@gmail.com>
2023-06-22 05:49:19 +0200

    struct 0.0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog{
+	char * name;
+	float age;
+	char * owner;
+};
