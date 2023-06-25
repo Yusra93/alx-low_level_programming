f27f9443cc60feb8868ea4d04d19d06065912310
Yusra93 <yusra.altigani@gmail.com>
2023-06-25 20:44:27 +0200

    struct 4.0

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * new_dog - Prints a struct dog
+ * @name: string
+ * @age: float
+ * @owner: string
+ * Return: pointer to a dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t *));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	if (!d)
+		return (NULL);
+	return (d);
+}
