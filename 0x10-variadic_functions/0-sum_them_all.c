998188506e6ffe787694bb383078c8873c8a726f
Yusra93 <yusra.altigani@gmail.com>
2023-07-01 11:39:42 +0200

    variadic 0.0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * int sum_them_all - a function that returns the sum of all its parameters.
+ * sum_them_all - a function that returns the sum of all its parameters.
  * @n: no of args
  * Return: sum of all args
  */
