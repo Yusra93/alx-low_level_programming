3e4e7fab3d8c73979e7689e723f0239a14cc75b6
Yusra93 <yusra.altigani@gmail.com>
2023-07-02 20:21:12 +0200

    Singly Linked List 4.0

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,12 +2,11 @@
 
 /**
  * free_list -  function that frees a list_t list.
- * @head : pointer to array of list_t nodes
- * @str : node value
- * Return: no of nodes
+ * @head : list_t node to br free
+ * Return: void
  */
 void free_list(list_t *head)
 {
+	free(head->str);
 	free(head);
-	head = NULL;
 }
