de39d5bfea3b9af3bc29f73ed4e70415d2580324
Yusra93 <yusra.altigani@gmail.com>
2023-07-02 17:45:06 +0200

    Singly Linked List 0.0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,4 +20,5 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
 #endif /* LISTS_H */
