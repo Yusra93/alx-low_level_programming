00b46cb48549e845c935f9ab49ae6ba679c9ce2b
Yusra93 <yusra.altigani@gmail.com>
2023-07-02 19:19:42 +0200

    Singly Linked List 3.0

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_node_end -  function that adds a new node at the end of a list_t list.
+ * @head : pointer to array of list_t nodes
+ * @str : node value
+ * Return: no of nodes
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int l = strlen(str);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = l;
+	new_node->next = NULL;
+	*head->next = new_node;
+	return (&new_node);
+}
