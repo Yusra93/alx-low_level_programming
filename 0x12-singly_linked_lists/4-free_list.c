58e1bfab485ba2083f18ea41d0acde048c059b5a
Yusra93 <yusra.altigani@gmail.com>
2023-07-02 20:44:09 +0200

    Singly Linked List 4.0

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,14 +7,8 @@
  */
 void free_list(list_t *head)
 {
-	list_t *t;
-
 	if (head == NULL)
 		exit(0);
-	while (head)
-	{
-	 	t = head->next);
-		free(head->str);
-		free(head);
-	}
+	free(head->str);
+	free(head);
 }
